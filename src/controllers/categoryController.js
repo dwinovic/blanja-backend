017d31385aef08bf174020ea758e2a0ea3e0fc23
dwinovic <nd.cahya19@gmail.com>
2021-07-22 19:19:11 +0700

    change linter from standardjs to eslint

--- src/controllers/categoryController.js
+++ src/controllers/categoryController.js
@@ -1,66 +1,66 @@
-const { response } = require('../helpers')
-const category = require('../models/category')
+const { response } = require('../helpers');
+const category = require('../models/category');
 
 const {
   createCategory,
   getAllCategory,
   getItemCategory,
   updateCategory,
-  deleteCategory
-} = category
+  deleteCategory,
+} = category;
 
 module.exports = {
   createCategory: (req, res) => {
-    const { nameCategory } = req.body
-    const data = { nameCategory }
+    const { nameCategory } = req.body;
+    const data = { nameCategory };
     // console.log(data);
     createCategory(data)
-      .then((result) => {
-        response(res, 200, data)
+      .then(() => {
+        response(res, 200, data);
       })
       .catch((err) => {
-        response(res, 500, {}, err)
-      })
+        response(res, 500, {}, err);
+      });
   },
   getAllCategory: (req, res) => {
     getAllCategory()
       .then((result) => {
-        response(res, 200, result)
+        response(res, 200, result);
       })
       .catch((err) => {
-        response(res, 500, {}, err)
-      })
+        response(res, 500, {}, err);
+      });
   },
   getItemCategory: (req, res) => {
-    const id = req.params.id
+    const id = req.params.id;
     getItemCategory(id)
       .then((result) => {
-        response(res, 200, result)
+        response(res, 200, result);
       })
       .catch((err) => {
-        response(res, 500, {}, err)
-      })
+        response(res, 500, {}, err);
+      });
   },
   updateCategory: (req, res) => {
-    const { nameCategory } = req.body
-    const id = req.params.id
-    const newData = { nameCategory, updatedAt: new Date() }
+    const { nameCategory } = req.body;
+    const id = req.params.id;
+    const newData = { nameCategory, updatedAt: new Date() };
     updateCategory(id, newData)
-      .then((result) => {
-        response(res, 200)
+      .then(() => {
+        response(res, 200);
       })
       .catch((err) => {
-        response(res, 500, {}, err)
-      })
+        response(res, 500, {}, err);
+      });
   },
   deleteCategory: (req, res) => {
-    const id = req.params.id
+    const id = req.params.id;
     deleteCategory(id)
-      .then((result) => {
-        response(res, 200)
+      .then(() => {
+        response(res, 200);
       })
       .catch((err) => {
-        response(res, 500, {}, err)
-      })
-  }
-}
+        response(res, 500, {}, err);
+      });
+  },
+};
